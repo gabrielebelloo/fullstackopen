84cec36522867e0a3518a40691e469b778e83ad9
Gabriele <briello2001@gmail.com>
2024-05-09 21:26:55 +0200

    Component state, event handlers

--- part1/exercise-test/src/App.jsx
+++ part1/exercise-test/src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onSmash, text}) => <button onClick={onSmash}>{text}</button>
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Display counter={counter} />
+      <Button 
+      onSmash={increaseByOne}
+      text='plus'
+      />
+      <Button 
+      onSmash={setToZero}
+      text='zero'
+      />
+      <Button
+      onSmash={decreaseByOne}
+      text='minus'
+      />
     </>
   )
 }
