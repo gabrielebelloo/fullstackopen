99b7446f181f3236504555ee0fefbf6c0a657e98
Gabriele <briello2001@gmail.com>
2024-05-11 18:48:14 +0200

    1.7: unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,16 +2,41 @@ import { useState } from 'react'
 
 const Button = ({text, handleClick}) => <button onClick={handleClick}>{text}</button>
 
-const Display = ({text, value}) => <div>{text} {value}</div>
+const Display = ({text, value, aftertext}) => <div>{text} {value} {aftertext}</div>
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
-  const increaseGood = () => setGood(good + 1);
-  const increaseNeutral = () => setNeutral(neutral + 1);
-  const increaseBad = () => setBad(bad + 1);
+  const increaseGood = () => {
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    const updatedAll = updatedGood + neutral + bad;
+    setAll(updatedAll);
+    setAverage((updatedGood - bad) / updatedAll);
+    setPositive(updatedGood / updatedAll * 100);
+  }
+
+  const increaseNeutral = () => {
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    const updatedAll = good + updatedNeutral + bad;
+    setAll(updatedAll);
+    setPositive(good / updatedAll * 100);
+  }
+
+  const increaseBad = () => {
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    const updatedAll = good + neutral + updatedBad;
+    setAll(updatedAll);
+    setAverage((good - updatedBad) / updatedAll);
+    setPositive(good / updatedAll * 100);
+  }
 
   return (
     <>
@@ -23,6 +48,9 @@ const App = () => {
       <Display text='good' value={good} />
       <Display text='neutral' value={neutral} />
       <Display text= 'bad' value={bad} />
+      <Display text='all' value={all} />
+      <Display text='average' value={average} />
+      <Display text='positive' value={positive} aftertext=' %'/>
     </>
   )
 }
