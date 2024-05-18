ea47beacef54c380cb1fdd1cf4e327c1e64b3f5c
Gabriele <briello2001@gmail.com>
2024-05-18 14:05:34 +0200

    Tests

--- part2/exercise-test/src/App.jsx
+++ part2/exercise-test/src/App.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react'
+import Note from './components/Note'
 
-const App = () => {
+const App = ({ notes }) => {
   return (
-    <p>Test</p>
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
   )
 }
 
