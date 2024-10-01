affbac65dd26135bdea9bdeb5cd4ae85a18a7332
Gabriele Bello <gabriele.bello@reddoak.com>
2024-10-01 12:42:34 +0200

    2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,52 +1,82 @@
-import Numbers from './components/Numbers'
-import { useState } from 'react'
+import Numbers from "./components/Numbers";
+import { useState } from "react";
 
-const App  = () => {
+const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 1 }
-  ])
-  const [newName, setNewName] = useState('')
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [filteredPersons, setFilteredPersons] = useState(persons);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newSearch, setNewSearch] = useState("");
 
   const addPerson = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const newPerson = {
       name: newName,
-      id: persons.length + 1
-    }
-    const found = persons.find((person) => person.name === newPerson.name)
+      number: newNumber,
+      id: persons.length + 1,
+    };
+    const found = persons.find((person) => person.name === newPerson.name);
 
     if (!found) {
-      const newPersons = persons.concat(newPerson)
-      setPersons(newPersons)
+      const newPersons = persons.concat(newPerson);
+      setPersons(newPersons);
+      setFilteredPersons(newPersons);
     } else {
-      alert(`${newName} is already added to phonebook`)
+      alert(`${newName} is already added to phonebook`);
     }
-    
-  }
+  };
 
-  const handleInput = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleInputName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleInputNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleInputSearch = (event) => {
+    const searchInput = event.target.value;
+    setNewSearch(searchInput);
+    if (newSearch) {
+      setFilteredPersons(
+        persons.filter((person) =>
+          person.name.toLowerCase().includes(searchInput.toLowerCase())
+        )
+      );
+    } else {
+      setFilteredPersons(persons);
+    }
+  };
 
   return (
     <>
       <h2>Phonebook</h2>
+      <div>
+        search: <input onChange={handleInputSearch} value={newSearch} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input onChange={handleInput} value={newName}/>
+          name: <input onChange={handleInputName} value={newName} />
         </div>
         <div>
-          <button type='submit'>
-            add
-          </button>
+          number: <input onChange={handleInputNumber} value={newNumber} />
+        </div>
+        <div>
+          <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        <Numbers persons={persons}/>
+        <Numbers persons={filteredPersons} />
       </ul>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part2/phonebook/src/components/Numbers.jsx
+++ part2/phonebook/src/components/Numbers.jsx
@@ -1,9 +1,11 @@
-const Number = ({person}) => <li>{person.name}</li>
+const Number = ({ person }) => (
+  <li>
+    {person.name} - {person.number}
+  </li>
+);
 
-const Numbers = ({persons}) => {
-  return (
-    persons.map((person) => <Number key={person.id} person={person} />)
-  )
-}
+const Numbers = ({ persons }) => {
+  return persons.map((person) => <Number key={person.id} person={person} />);
+};
 
-export default Numbers
+export default Numbers;
