894eac1f44ad521c5e5fef78861ddd0df3635166
Gabriele Bello <gabriele.bello@reddoak.com>
2024-10-04 15:13:38 +0200

    3.9 Phonebook backend step 9

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('dist'));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
 morgan.token('content', (req, res) => {
     return JSON.stringify(req.body);
