cc030713c8f8ea90545de83ffd45a94a3b7fcf11
Gabriele Bello <gabriele.bello@reddoak.com>
2024-10-04 11:40:11 +0200

    Notes frontend

---
+++ part3/notes__frontend/src/App.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react";
+import noteService from "./services/note";
+import Note from "./components/Note";
+import Notification from "./components/Notification";
+
+const App = (props) => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    noteService.getAll().then((res) => setNotes(res));
+  }, []);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    };
+    noteService.create(noteObject).then((res) => {
+      setNotes(notes.concat(res));
+      setNewNote("");
+    });
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService.update(id, changedNote).then((res) => {
+      setNotes(notes.map((n) => (n.id !== id ? n : res)))})
+        .catch((error) => {
+          setErrorMessage(error);
+          setTimeout(() => {
+            setErrorMessage(null);
+          }, 5000);
+        });
+  };
+
+  return (
+    <>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+    </div>
+  )
+}
