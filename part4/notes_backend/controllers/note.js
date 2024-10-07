658a47a9980f497e65062b5ad8eb884e34edb46f
Gabriele Bello <gabriele.bello@reddoak.com>
2024-10-07 11:39:52 +0200

    Project structure

---
+++ part4/notes_backend/controllers/note.js
@@ -0,0 +1,70 @@
+const notesRouter = require("express").Router();
+const Note = require("../models/note");
+
+
+notesRouter.post("/", (request, response, next) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: Boolean(body.important) || false,
+  });
+
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((err) => next(err));
+});
+
+notesRouter.get("/", (request, response) => {
+  Note.find({}).then((result) => {
+    response.json(result);
+  });
+});
+
+app.get(":id", (request, response, next) => {
+  const id = request.params.id;
+  Note.findById(id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((err) => next(err));
+});
+
+notesRouter.delete(":id", (request, response, next) => {
+  const id = request.params.id;
+  Note.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((err) => next(err));
+});
+
+notesRouter.put(":id", (request, response, next) => {
+  const id = request.params.id;
+  const { content, important } = request.body;
+
+  Note.findByIdAndUpdate(
+    id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((err) => next(err));
+});
+
+module.exports = notesRouter;
--- part4/notes_backend/models/note.js
+++ part4/notes_backend/models/note.js
@@ -1,17 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const url = process.env.MONGODB_URI;
-
-mongoose.set('strictQuery', false);
-
-mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    });
 
 const noteSchema = new mongoose.Schema({
     content: {
