3093c70d6f373fc9783804ae4b0c224c52bf189d
Gabriele Bello <gabriele.bello@reddoak.com>
2024-10-18 15:39:08 +0200

    tests

--- part4/bloglist_be/utils/list_helper.js
+++ part4/bloglist_be/utils/list_helper.js
@@ -1,7 +1,21 @@
+const blog = require("../models/blog");
+
 const dummy = (blogs) => {
   return 1;
-}
+};
+
+const totalLikes = (blogs) => {
+  let totalLikes = 0;
+  if (blogs) {
+    blogs.forEach((blog) => {
+      totalLikes += blog.like;
+    });
+    console.log(totalLikes);
+  }
+  return totalLikes;
+};
 
 module.exports = {
-  dummy
-}
+  dummy,
+  totalLikes,
+};
