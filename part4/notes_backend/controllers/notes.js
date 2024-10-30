20c45731441f3d354a62379dad609bc38d29a900
Gabriele Bello <gabriele.bello@reddoak.com>
2024-10-30 15:25:04 +0100

    Error handling and async/await Eliminating the try-catch

--- part4/notes_backend/controllers/notes.js
+++ part4/notes_backend/controllers/notes.js
@@ -5,12 +5,6 @@ const Note = require("../models/note");
 notesRouter.post("/", async (request, response, next) => {
   const body = request.body;
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
-
   const note = new Note({
     content: body.content,
     important: Boolean(body.important) || false,
@@ -25,27 +19,19 @@ notesRouter.get("/", async(request, response) => {
   response.json(notes);
 });
 
-notesRouter.get(":id", (request, response, next) => {
-  const id = request.params.id;
-  Note.findById(id)
-    .then((note) => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((err) => next(err));
-});
+notesRouter.get('/:id', async (request, response, next) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
 
-notesRouter.delete(":id", (request, response, next) => {
-  const id = request.params.id;
-  Note.findByIdAndDelete(id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch((err) => next(err));
-});
+notesRouter.delete('/:id', async (request, response, next) => {
+  await Note.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
 
 notesRouter.put(":id", (request, response, next) => {
   const id = request.params.id;
